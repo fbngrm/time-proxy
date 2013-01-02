c8df2ec0559b6de36d9e3e1ea99bb84383c5bc8b
fgrimme <fbngrm@gmail.com>
2013-01-02 15:09:36 +0100

    add readme

---
+++ README.mk
@@ -0,0 +1,1 @@
+Node.js decorator/proxy for timing measurement of node.js modules. Enables timing measurement of every method of the decorated object. For testing use mocha [npm install mocha] and should [npm install should]
